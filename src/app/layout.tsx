bbaeff4470d4b1a966108bf1c89f7223b3852fdf
Romain Pierucci <pierucci.romain@gmail.com>
2023-05-18 10:58:36 +0200

    feat: custom theme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { NextUIProvider } from '@nextui-org/react';
+import { NextUIProvider, createTheme } from '@nextui-org/react';
 import { Poppins } from 'next/font/google';
 import * as React from 'react';
 import './globals.scss';
@@ -11,6 +11,32 @@ const poppins = Poppins({
   display: 'swap',
 });
 
+const lightTheme = createTheme({
+  type: 'light',
+  theme: {
+    colors: {
+      primaryLight: '$green200',
+      primaryLightHover: '$green300',
+      primaryLightActive: '$green400',
+      primaryLightContrast: '$green600',
+      primary: '#4ADE7B',
+      primaryBorder: '$green500',
+      primaryBorderHover: '$green600',
+      primarySolidHover: '$green700',
+      primarySolidContrast: '$white',
+      primaryShadow: '$green500',
+
+      gradient:
+        'linear-gradient(112deg, $blue100 -25%, $pink500 -10%, $purple500 80%)',
+      link: '#5E1DAD',
+
+      myColor: '#ff4ecd',
+    },
+    space: {},
+    fonts: {},
+  },
+});
+
 type RootLayoutProps = {
   children: React.ReactNode;
 };
@@ -19,7 +45,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="fr">
       <body className={poppins.className}>
-        <NextUIProvider>{children}</NextUIProvider>
+        <NextUIProvider theme={lightTheme}>{children}</NextUIProvider>
       </body>
     </html>
   );
