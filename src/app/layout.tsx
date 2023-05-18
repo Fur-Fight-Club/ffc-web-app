ef4a897d8d5ca9c3c476ef536320037c25fa7fd1
Romain Pierucci <pierucci.romain@gmail.com>
2023-05-18 11:05:05 +0200

    feat: light theme and dark theme conf

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { NextUIProvider, createTheme } from '@nextui-org/react';
+import { ThemeProvider as NextThemesProvider } from 'next-themes';
 import { Poppins } from 'next/font/google';
 import * as React from 'react';
 import './globals.scss';
@@ -37,6 +38,32 @@ const lightTheme = createTheme({
   },
 });
 
+const darkTheme = createTheme({
+  type: 'light',
+  theme: {
+    colors: {
+      primaryLight: '$green200',
+      primaryLightHover: '$green300',
+      primaryLightActive: '$green400',
+      primaryLightContrast: '$green600',
+      primary: '#4ADE7B',
+      primaryBorder: '$green500',
+      primaryBorderHover: '$green600',
+      primarySolidHover: '$green700',
+      primarySolidContrast: '$white',
+      primaryShadow: '$green500',
+
+      gradient:
+        'linear-gradient(112deg, $blue100 -25%, $pink500 -10%, $purple500 80%)',
+      link: '#5E1DAD',
+
+      myColor: '#ff4ecd',
+    },
+    space: {},
+    fonts: {},
+  },
+});
+
 type RootLayoutProps = {
   children: React.ReactNode;
 };
@@ -45,7 +72,17 @@ export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="fr">
       <body className={poppins.className}>
-        <NextUIProvider theme={lightTheme}>{children}</NextUIProvider>
+        <NextThemesProvider
+          defaultTheme="system"
+          attribute="class"
+          value={{
+            light: lightTheme.className,
+            dark: darkTheme.className,
+          }}
+        >
+          <NextUIProvider theme={lightTheme}>{children}</NextUIProvider>
+        </NextThemesProvider>
+        ;
       </body>
     </html>
   );
