32009f9de6803090af880183f3e8dbeaaa669640
Romain Pierucci <pierucci.romain@gmail.com>
2023-05-29 23:29:58 +0200

    feat(zodSchema): update user schema

--- src/model/user.schema.ts
+++ src/model/user.schema.ts
@@ -6,23 +6,28 @@ const userSchema = z.object({
   id: z.number(),
   firstname: z
     .string()
-    .min(2, { message: 'Le prénom doit comporter au moins 2 caractères' })
-    .max(255)
-    .regex(/^[A-Za-z]+$/, {
-      message: 'Le prénom doit être composé uniquement de lettres',
-    }),
-
+    .min(2, { message: 'Firstname must be at least 2 characters long' })
+    .max(255),
   lastname: z
     .string()
-    .min(2, { message: 'Le nom doit comporter au moins 2 caractères' })
-    .max(255)
-    .regex(/^[A-Za-z]+$/, {
-      message: 'Le nom doit être composé uniquement de lettres',
+    .min(2, { message: 'Lastname must be at least 2 characters long' })
+    .max(255),
+  email: z.string().email({ message: 'Invalid email address' }),
+  password: z
+    .string()
+    .min(8, { message: 'Password must be at least 8 characters long' })
+    .refine((password) => /[A-Z]/.test(password), {
+      message: 'Password must contain at least one uppercase character',
+    })
+    .refine((password) => /[a-z]/.test(password), {
+      message: 'Password must contain at least one lowercase character',
+    })
+    .refine((password) => /[0-9]/.test(password), {
+      message: 'Password must contain at least one digit',
+    })
+    .refine((password) => /[^a-zA-Z0-9]/.test(password), {
+      message: 'Password must contain at least one special character',
     }),
-  email: z.string().email({ message: 'Adresse électronique invalide' }),
-  password: z.string().min(8, {
-    message: 'Le mot de passe doit comporter au moins 8 caractères',
-  }),
   role: roleEnum,
 });
 
