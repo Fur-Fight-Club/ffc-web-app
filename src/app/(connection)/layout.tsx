6c7df14ce14d1e6b653d1c5aec2e5cd7ceba85ad
Romain Pierucci <pierucci.romain@gmail.com>
2023-05-29 16:06:32 +0200

    feat(Login): add images in public folder + upgrade login page with links + change main theme

--- src/app/(connection)/layout.tsx
+++ src/app/(connection)/layout.tsx
@@ -1,7 +1,11 @@
 'use client';
 
 import fireAnimation from '@assets/animations/fire.json';
+import { CaretLeft } from '@phosphor-icons/react';
 import Lottie from 'lottie-react';
+import Image from 'next/image';
+import Link from 'next/link';
+import { useRouter } from 'next/navigation';
 import styles from './layout.module.scss';
 
 type LoginLayoutProps = {
@@ -9,9 +13,30 @@ type LoginLayoutProps = {
 };
 
 export default function LoginLayout({ children }: LoginLayoutProps) {
+  const router = useRouter();
+
   return (
     <main className={styles.wrapper}>
-      <div className={styles.contentPanel}>{children}</div>
+      <div className={styles.contentPanel}>
+        <nav className={styles.navbar}>
+          <a onClick={router.back} className={styles.goBack}>
+            <CaretLeft size={32} weight="bold" />
+          </a>
+
+          <Link href="/">
+            <Image
+              className={styles.logo}
+              src="/images/ffc-logo.svg"
+              width={75}
+              height={75}
+              alt="logo"
+            />
+          </Link>
+
+          <div></div>
+        </nav>
+        {children}
+      </div>
       <div className={styles.imgPanel}>
         <Lottie animationData={fireAnimation} />
       </div>
