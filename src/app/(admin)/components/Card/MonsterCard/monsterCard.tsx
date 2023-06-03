c58ba320bff1cc6a290e888b670e94d3583fc282
SylvainBoudacher <sylvain.boudacher@gmail.com>
2023-06-03 22:57:35 +0200

    refactor(monsterCard): parse Weight category

--- src/app/(admin)/components/Card/MonsterCard/monsterCard.tsx
+++ src/app/(admin)/components/Card/MonsterCard/monsterCard.tsx
@@ -1,6 +1,7 @@
 'use client';
 import { Card, Col, Row, Text } from '@nextui-org/react';
 import { Monster } from 'src/store/monsters/monsters.model';
+import { parseWheightCategory } from 'src/utils/utils';
 
 export const MonsterCard = (props: { monster: Monster }) => {
   const { monster } = props;
@@ -10,16 +11,16 @@ export const MonsterCard = (props: { monster: Monster }) => {
       <Card.Header css={{ position: 'absolute', zIndex: 1, top: 5 }}>
         <Col>
           <Text size={12} weight="bold" transform="uppercase" color="#ffffffAA">
-            Name
+            Nom
           </Text>
           <Text h3 color="black">
-            {monster.name}
+            {monster?.name}
           </Text>
         </Col>
       </Card.Header>
       <Card.Body css={{ p: 0 }}>
         <Card.Image
-          src={monster.picture}
+          src={monster?.picture}
           width="100%"
           height="100%"
           objectFit="cover"
@@ -39,10 +40,10 @@ export const MonsterCard = (props: { monster: Monster }) => {
         <Row>
           <Col>
             <Text color="#000" size={12}>
-              {monster.weight_category}
+              {parseWheightCategory(monster?.weight_category)}
             </Text>
             <Text color="#000" size={12}>
-              {monster.weight} KG
+              {monster?.weight} KG
             </Text>
           </Col>
           <Col></Col>
