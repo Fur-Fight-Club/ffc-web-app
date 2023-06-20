d6818df255d823936d9994d9f9acee586b3137ab
Milan <pro@turtletv.fr>
2023-06-20 19:11:46 +0200

    ✨ Template component

---
+++ src/components/UI/Template/Template.component.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import * as React from "react";
+// @ts-ignore
+import styles from "./Template.module.scss";
+
+interface TemplateProps {}
+
+export const Template: React.FunctionComponent<TemplateProps> = ({}) => {
+  return <div></div>;
+};
