f80194c24177adec58997d3cc24db9218e566d10
ZERRAI Sami <sami.zerrai@gmail.com>
2023-06-23 22:47:39 +0200

    move monster in (user)

--- src/app/(user)/monster/create/page.tsx
+++ src/app/(user)/monster/create/page.tsx
@@ -6,11 +6,7 @@ import Divider from "@components/UI/Divider";
 import { Row, Spacer, Text, Col, Input } from "@nextui-org/react";
 import styles from "./page.module.scss";
 import React, { useState } from "react";
-import {
-  monsterType,
-  weightCategories,
-  convertApiTypeToType,
-} from "../../utils";
+import { monsterType, weightCategories, convertApiTypeToType } from "../utils";
 import { Select } from "antd";
 import { applicationState } from "src/store/application/selector";
 import { useSelector } from "react-redux";
