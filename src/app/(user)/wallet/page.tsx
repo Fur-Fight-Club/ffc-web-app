37a3f340181139682768398d4a7502a24885bef2
Romain Pierucci <pierucci.romain@gmail.com>
2023-06-23 23:24:56 +0200

    feat(wallet): create wallet space + KPI component card + Table of transactions

--- src/app/(user)/wallet/page.tsx
+++ src/app/(user)/wallet/page.tsx
@@ -1,6 +1,8 @@
 "use client";
 
-import { Table, Text } from "@nextui-org/react";
+import { Row, Spacer, Table, Text } from "@nextui-org/react";
+import colors from "@styles/_colors.module.scss";
+import KpiCard from "./components/KpiCard/KpiCard";
 
 type WalletPageProps = {};
 
@@ -49,7 +51,34 @@ const WalletPage = (props: WalletPageProps) => {
   return (
     <div>
       <Text h2 size={"$lg"}>
-        Transations
+        Données financières
+      </Text>
+      <Row>
+        <KpiCard
+          amount={30.9}
+          label="Total"
+          color={colors.secondaryT500}
+          unityLabel="€"
+        />
+        <Spacer x={1} />
+        <KpiCard
+          amount={30.9}
+          label="Jetons gagnés"
+          color={colors.successLight}
+          contratsColor={colors.success}
+          unityLabel="Jetons"
+        />
+        <Spacer x={1} />
+        <KpiCard
+          amount={30.9}
+          label="Jetons perdus"
+          color={colors.dangerLight}
+          contratsColor={colors.danger}
+          unityLabel="Jetons"
+        />
+      </Row>
+      <Text h2 size={"$lg"}>
+        Historique des transactions
       </Text>
       <Table
         aria-label="Example table with dynamic content"
@@ -66,7 +95,7 @@ const WalletPage = (props: WalletPageProps) => {
         <Table.Body items={rows}>
           {(item) => (
             <Table.Row key={item.key}>
-              {(columnKey) => <Table.Cell>{item[columnKey]}</Table.Cell>}
+              {(columnKey) => <Table.Cell>pouet</Table.Cell>}
             </Table.Row>
           )}
         </Table.Body>
