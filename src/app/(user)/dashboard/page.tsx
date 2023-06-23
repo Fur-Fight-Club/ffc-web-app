ee14fe666fc757afcf72477f5081d3885967c024
Romain Pierucci <pierucci.romain@gmail.com>
2023-06-23 20:01:34 +0200

    feat: user card

--- src/app/(user)/dashboard/page.tsx
+++ src/app/(user)/dashboard/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { Grid } from "@nextui-org/react";
+import UserOverview from "./components/UserOverview/UserOverview";
 import styles from "./page.module.scss";
 
 type DashboardProps = {};
@@ -9,7 +10,9 @@ const Dashboard = (props: DashboardProps) => {
   return (
     <div className={styles.dashboardContainer}>
       <Grid.Container gap={2} justify="space-between">
-        <Grid xs={4}>pouet</Grid>
+        <Grid xs={4}>
+          <UserOverview fullname="Adrien Morin" inscriptionDate="" />
+        </Grid>
         <Grid xs={4}>pouet</Grid>
         <Grid xs={4}>pouet</Grid>
         <Grid xs={4}>pouet</Grid>
