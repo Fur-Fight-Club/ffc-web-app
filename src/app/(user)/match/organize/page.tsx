97416393c2497bc4a46bbd5d8017d1de0cf6fad6
Romain Pierucci <pierucci.romain@gmail.com>
2023-06-26 18:59:50 +0200

    feat(match): make choice between participate or create a match

---
+++ src/app/(user)/match/organize/page.tsx
@@ -0,0 +1,7 @@
+type OrganizeMatchPageProps = {};
+
+const OrganizeMatchPage = (props: OrganizeMatchPageProps) => {
+  return <h1>Organize Match Page</h1>;
+};
+
+export default OrganizeMatchPage;
--- src/app/(user)/match/page.tsx
+++ src/app/(user)/match/page.tsx
@@ -1,11 +1,55 @@
+"use client";
+
+import { Spacer, Text } from "@nextui-org/react";
+import { motion } from "framer-motion";
+import { useRouter } from "next/navigation";
+import colors from "src/styles/_colors.module.scss";
+import styles from "./page.module.scss";
+
 type MatchPageProps = {};
 
 const MatchPage = (props: MatchPageProps) => {
-  const {} = props;
+  const router = useRouter();
 
   return (
-    <div>
-      <h1>Matchs</h1>
+    <div className={styles.wrapper}>
+      <motion.div
+        className={styles.leftPanel}
+        onClick={() => router.push("/match/participate")}
+        initial={{
+          x: "-100%",
+        }}
+        animate={{ x: 0 }}
+        whileHover={{ scale: 1.05 }}
+      >
+        <div className={styles.textContent}>
+          <Text h2 size={"$6xl"} weight={"black"} color={colors.white}>
+            Participez
+          </Text>
+          <Text h3 size={"$xl"} weight={"black"} color={colors.white}>
+            Défier un adversaire sur son propre terrain
+          </Text>
+        </div>
+      </motion.div>
+      <Spacer x={1} />
+      <motion.div
+        className={styles.rightPanel}
+        onClick={() => router.push("/match/organize")}
+        initial={{
+          x: "100%",
+        }}
+        animate={{ x: 0 }}
+        whileHover={{ scale: 1.05 }}
+      >
+        <div className={styles.textContent}>
+          <Text h2 size={"$6xl"} weight={"black"} color={colors.primary}>
+            Organiser
+          </Text>
+          <Text h3 size={"$xl"} weight={"black"} color={colors.primary}>
+            Décidez du lieu, date et montant à gagner
+          </Text>
+        </div>
+      </motion.div>
     </div>
   );
 };
