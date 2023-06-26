25f739aa34c7d80443e5fad6504220ee8cae0628
Romain Pierucci <pierucci.romain@gmail.com>
2023-06-26 20:25:09 +0200

    feat(match): layout of step 1 of organise match

--- src/app/(user)/match/organize/page.tsx
+++ src/app/(user)/match/organize/page.tsx
@@ -1,7 +1,27 @@
+"use client";
+
+import CardList from "@components/CardList/CardList";
+import { Grid } from "@nextui-org/react";
+
 type OrganizeMatchPageProps = {};
 
 const OrganizeMatchPage = (props: OrganizeMatchPageProps) => {
-  return <h1>Organize Match Page</h1>;
+  return (
+    <Grid.Container css={{ height: "100%" }}>
+      <Grid xs={4}>
+        <div style={{ width: "100%" }}>
+          <div>Vos monstres</div>
+          <CardList />
+        </div>
+      </Grid>
+      <Grid xs={4}>
+        <div>Match</div>
+      </Grid>
+      <Grid xs={4}>
+        <div>Match</div>
+      </Grid>
+    </Grid.Container>
+  );
 };
 
 export default OrganizeMatchPage;
