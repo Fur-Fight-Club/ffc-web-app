bebdaa070935f494475cbca0a38f7a2ba7b3cd7e
Romain Pierucci <pierucci.romain@gmail.com>
2023-06-26 16:32:10 +0200

    feat(match): add match section + update link in navbar

---
+++ src/app/(user)/match/page.tsx
@@ -0,0 +1,13 @@
+type MatchPageProps = {};
+
+const MatchPage = (props: MatchPageProps) => {
+  const {} = props;
+
+  return (
+    <div>
+      <h1>Matchs</h1>
+    </div>
+  );
+};
+
+export default MatchPage;
