1b9a3af0fcef92f8e3ec143ad9e0ea3e0ad70a23
Romain Pierucci <pierucci.romain@gmail.com>
2023-06-28 20:22:17 +0200

    feat(match): refactor step 1

--- src/app/(user)/match/organize/page.tsx
+++ src/app/(user)/match/organize/page.tsx
@@ -1,84 +1,20 @@
 "use client";
 
-import CardList from "@components/CardList/components/CardList";
-import { Button, Grid, Row, Spacer, Text } from "@nextui-org/react";
-import colors from "@styles/_colors.module.scss";
-import { useDispatch, useSelector } from "react-redux";
-import { applicationState } from "src/store/application/selector";
+import { useSelector } from "react-redux";
 import { createMatchFormState } from "src/store/matches/selector";
-import { setMonsterCreateForm } from "src/store/matches/slice";
-import { Monster } from "src/store/monsters/monsters.model";
-import MonsterCardDetails from "./components/MonsterCardDetails/MonsterCardDetails";
-import MonsterCardPreview from "./components/MonsterCardPreview/MonsterCardPreview";
+import Step1 from "./components/Step1";
 
 type OrganizeMatchPageProps = {};
 
 const OrganizeMatchPage = (props: OrganizeMatchPageProps) => {
-  const dispatch = useDispatch();
-
-  const { user } = useSelector(applicationState);
-  const monsters = user?.Monster;
-
   const { monster, step, arena, bet } = useSelector(createMatchFormState);
 
-  const handleOnClick = (selectedMonster: Monster) => {
-    monster?.id === selectedMonster.id
-      ? dispatch(setMonsterCreateForm(null))
-      : dispatch(setMonsterCreateForm(selectedMonster));
-  };
-
-  return (
-    <div style={{ height: "95%" }}>
-      <Grid.Container css={{ height: "100%", position: "relative" }} gap={2}>
-        <Grid xs={4}>
-          <div style={{ width: "100%" }}>
-            <div>Vos monstres</div>
-            <CardList>
-              {monsters?.map((monster) => (
-                <CardList.MonsterItem
-                  key={monster.id}
-                  monster={monster}
-                  onClick={() => handleOnClick(monster)}
-                />
-              ))}
-            </CardList>
-          </div>
-        </Grid>
-        <Grid xs={4}>
-          <div style={{ width: "100%" }}>
-            <div>Fiche</div>
-            <MonsterCardDetails monster={monster} />
-          </div>
-        </Grid>
-        <Grid xs={4}>
-          <div
-            style={{
-              width: "100%",
-              display: "flex",
-              flexDirection: "column",
-              justifyContent: "center",
-              alignItems: "center",
-            }}
-          >
-            <Text
-              h3
-              size={"$lg"}
-              css={{ textAlign: "center" }}
-              color={colors.primaryT300}
-            >
-              Votre combattant :
-            </Text>
-            <MonsterCardPreview monster={monster} />
-          </div>
-        </Grid>
-      </Grid.Container>
-      <Row justify="flex-end">
-        <Button bordered>Retour</Button>
-        <Spacer x={0.5} />
-        <Button>Suivant</Button>
-      </Row>
-    </div>
-  );
+  switch (step) {
+    case 0:
+      return <Step1 />;
+    default:
+      return <div>default steps forms</div>;
+  }
 };
 
 export default OrganizeMatchPage;
