63cf646de9447c55776820eda57960584f20ce85
Milan <pro@turtletv.fr>
2023-06-29 02:58:29 +0200

    8^()

--- src/app/(user)/match/organize/page.tsx
+++ src/app/(user)/match/organize/page.tsx
@@ -1,55 +1,23 @@
 "use client";
 
-import CardList from "@components/CardList/components/CardList";
-import { Grid } from "@nextui-org/react";
-import { toast } from "react-hot-toast";
-import { useDispatch, useSelector } from "react-redux";
-import { applicationState } from "src/store/application/selector";
+import { useSelector } from "react-redux";
 import { createMatchFormState } from "src/store/matches/selector";
-import { setMonsterCreateForm } from "src/store/matches/slice";
-import { Monster } from "src/store/monsters/monsters.model";
+import Step1 from "./components/Step1";
+import Step2 from "./components/Step2";
 
 type OrganizeMatchPageProps = {};
 
 const OrganizeMatchPage = (props: OrganizeMatchPageProps) => {
-  const dispatch = useDispatch();
-
-  const { user } = useSelector(applicationState);
   const { monster, step, arena, bet } = useSelector(createMatchFormState);
 
-  const monsters = user?.Monster;
-  console.log("monsters", monsters);
-
-  const handleOnClick = (monster: Monster) => {
-    console.log("set Monster");
-    dispatch(setMonsterCreateForm(monster));
-    toast.success("Monster ajouté dans le store");
-  };
-
-  return (
-    <Grid.Container css={{ height: "100%" }}>
-      <Grid xs={4}>
-        <div style={{ width: "100%" }}>
-          <div>Vos monstres</div>
-          <CardList>
-            {monsters?.map((monster) => (
-              <CardList.MonsterItem
-                key={monster.id}
-                monster={monster}
-                onClick={() => handleOnClick(monster)}
-              />
-            ))}
-          </CardList>
-        </div>
-      </Grid>
-      <Grid xs={4}>
-        <div style={{ width: "100%" }}>Match</div>
-      </Grid>
-      <Grid xs={4}>
-        <div>Match</div>
-      </Grid>
-    </Grid.Container>
-  );
+  switch (step) {
+    case 0:
+      return <Step1 />;
+    case 1:
+      return <Step2 />;
+    default:
+      return <div>default steps forms</div>;
+  }
 };
 
 export default OrganizeMatchPage;
