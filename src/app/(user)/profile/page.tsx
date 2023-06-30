3227d7b8ef83420ad71b058b4209d2c23c53b1db
ZERRAI Sami <sami.zerrai@gmail.com>
2023-07-01 00:25:05 +0200

    ✨ profile

--- src/app/(user)/profile/page.tsx
+++ src/app/(user)/profile/page.tsx
@@ -3,11 +3,35 @@
 import { Card, Col, Row, Text, Button, Spacer } from "@nextui-org/react";
 import { useSelector } from "react-redux";
 import { applicationState } from "src/store/application/selector";
+import colors from "@styles/_colors.module.scss";
+import { CaretRight } from "@phosphor-icons/react";
+import { useState } from "react";
+import { Input } from "antd";
 
 type ProfilePageProps = {};
 
 const ProfilePage = (props: ProfilePageProps) => {
   const { user } = useSelector(applicationState);
+  const [visibleFormFirstname, setVisibleFormFirstname] = useState("none");
+  const [visibleFormLastname, setVisibleFormLastname] = useState("none");
+  const [visibleFormEmail, setVisibleFormEmail] = useState("none");
+  const [visibleFormPassword, setVisibleFormPassword] = useState("none");
+
+  const handleVisibleFormFirstname = () => {
+    setVisibleFormFirstname(visibleFormFirstname === "none" ? "flex" : "none");
+  };
+
+  const handleVisibleFormLastname = () => {
+    setVisibleFormLastname(visibleFormLastname === "none" ? "flex" : "none");
+  };
+
+  const handleVisibleFormEmail = () => {
+    setVisibleFormEmail(visibleFormEmail === "none" ? "flex" : "none");
+  };
+
+  const handleVisibleFormPassword = () => {
+    setVisibleFormPassword(visibleFormPassword === "none" ? "flex" : "none");
+  };
 
   return (
     <div>
@@ -38,21 +62,117 @@ const ProfilePage = (props: ProfilePageProps) => {
               </Row>
             </Card.Header>
             <Card.Divider />
-            <Card.Body>
-              <Row align="center" css={{ m: "$7" }}>
+            <Card.Body css={{ p: "inherit" }}>
+              <Row
+                align="center"
+                css={{
+                  p: "$7",
+                  display: visibleFormFirstname === "flex" ? "none" : "flex",
+                }}
+              >
                 <Text>Prénom</Text>
                 <Spacer x={10} />
                 <Text weight={"bold"}>{user.firstname}</Text>
+                <Spacer x={10} />
+                <Row align="flex-end" justify="flex-end">
+                  <Button
+                    auto
+                    css={{ background: "none" }}
+                    onClick={() => handleVisibleFormFirstname()}
+                  >
+                    <CaretRight size={20} color={colors.black} />
+                  </Button>
+                </Row>
               </Row>
-              <Row align="center" css={{ m: "$7" }}>
+              <Row
+                align="center"
+                css={{
+                  p: "$7",
+                  background: colors.secondaryT500,
+                  display: visibleFormFirstname,
+                }}
+              >
+                <Text>Prénom</Text>
+                <Spacer x={10} />
+                <Input placeholder="Prénom" value={user.firstname} />{" "}
+                <Spacer x={10} />
+                <Row align="flex-end" justify="flex-end">
+                  <Button auto>Valider</Button>
+                </Row>
+              </Row>
+              <Row
+                align="center"
+                css={{
+                  p: "$7",
+                  display: visibleFormLastname === "flex" ? "none" : "flex",
+                }}
+              >
                 <Text>Nom</Text>
                 <Spacer x={10} />
                 <Text weight={"bold"}>{user.lastname}</Text>
+                <Spacer x={10} />
+                <Row align="flex-end" justify="flex-end">
+                  <Button
+                    auto
+                    css={{ background: "none" }}
+                    onClick={() => handleVisibleFormLastname()}
+                  >
+                    <CaretRight size={20} color={colors.black} />
+                  </Button>
+                </Row>
               </Row>
-              <Row align="center" css={{ m: "$7" }}>
+              <Row
+                align="center"
+                css={{
+                  p: "$7",
+                  background: colors.secondaryT500,
+                  display: visibleFormLastname,
+                }}
+              >
+                <Text>Nom</Text>
+                <Spacer x={10} />
+                <Input placeholder="Prénom" value={user.lastname} />{" "}
+                <Spacer x={10} />
+                <Row align="flex-end" justify="flex-end">
+                  <Button auto>Valider</Button>
+                </Row>
+              </Row>
+              <Row
+                align="center"
+                css={{
+                  p: "$7",
+                  display: visibleFormEmail === "flex" ? "none" : "flex",
+                }}
+              >
                 <Text>Email</Text>
                 <Spacer x={10} />
                 <Text weight={"bold"}>{user.email}</Text>
+                <Spacer x={10} />
+                <Row align="flex-end" justify="flex-end">
+                  <Button
+                    auto
+                    css={{ background: "none" }}
+                    onClick={() => handleVisibleFormEmail()}
+                  >
+                    <CaretRight size={20} color={colors.black} />
+                  </Button>
+                </Row>
+              </Row>
+              <Row
+                align="center"
+                css={{
+                  p: "$7",
+                  background: colors.secondaryT500,
+                  display: visibleFormEmail,
+                }}
+              >
+                <Text>Email</Text>
+                <Spacer x={10} />
+                <Input placeholder="Prénom" value={user.email} />{" "}
+                <Spacer x={10} />
+                <Row align="flex-end" justify="flex-end">
+                  <Button auto>Valider</Button>
+                </Row>
               </Row>
             </Card.Body>
           </Card>
@@ -71,10 +191,58 @@ const ProfilePage = (props: ProfilePageProps) => {
             </Card.Header>
             <Card.Divider />
             <Card.Body>
-              <Row align="center" css={{ m: "$7" }}>
-                <Text>Mot de passe</Text>
+              <Row
+                align="center"
+                css={{
+                  p: "$7",
+                  display: visibleFormPassword === "flex" ? "none" : "flex",
+                }}
+              >
+                <Text style={{ width: "20rem" }}>Mot de passe</Text>
                 <Spacer x={10} />
-                <Text weight={"bold"}>*********</Text>
+                <Text weight={"bold"}>********</Text>
+                <Spacer x={10} />
+                <Row align="flex-end" justify="flex-end">
+                  <Button
+                    auto
+                    css={{ background: "none" }}
+                    onClick={() => handleVisibleFormPassword()}
+                  >
+                    <CaretRight size={20} color={colors.black} />
+                  </Button>
+                </Row>
+              </Row>
+              <Row
+                align="center"
+                css={{
+                  p: "$7",
+                  background: colors.secondaryT500,
+                  display: visibleFormPassword,
+                }}
+              >
+                <Text style={{ width: "30rem" }}>Mot de passe</Text>
+                <Spacer x={5} />
+                <Col>
+                  <Input
+                    style={{ margin: "0.5rem" }}
+                    width={"30rem"}
+                    placeholder="Ancien mot de passe"
+                  />
+                  <Input
+                    style={{ margin: "0.5rem" }}
+                    width={"30rem"}
+                    placeholder="Nouveau mot de passe"
+                  />
+                  <Input
+                    style={{ margin: "0.5rem" }}
+                    width={"30rem"}
+                    placeholder="Confirmer mot de passe"
+                  />
+                </Col>
+                <Spacer x={5} />
+                <Row align="flex-end" justify="flex-end">
+                  <Button auto>Valider</Button>
+                </Row>
               </Row>
             </Card.Body>
           </Card>
