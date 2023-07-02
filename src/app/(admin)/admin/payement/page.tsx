8e5708c99143c4766357dbb381d2b052de0ea135
Sulyk <sylvain.boudacher@gmail.com>
2023-07-02 17:08:31 +0200

    add second tables

--- src/app/(admin)/admin/payement/page.tsx
+++ src/app/(admin)/admin/payement/page.tsx
@@ -1,27 +1,17 @@
 "use client";
 
-import { Button, Row, Spacer, Table, Text } from "@nextui-org/react";
 import { convertTokenToAmount } from "@utils/utils";
 import {
   TransactionTag,
   TransactionType,
 } from "ffc-prisma-package/dist/client";
-import { motion } from "framer-motion";
 import { useEffect, useState } from "react";
 import { useGetAllPayementQuery } from "src/store/payments/slice";
 
+import { Button, Row, Spacer, Table, Text } from "@nextui-org/react";
+import { motion } from "framer-motion";
+
 export default function ArenaAdmin() {
-  const [selectedPart, setSelectedPart] = useState<number>(1);
-  const [visibleModal, setVisibleModal] = useState(false);
-
-  const { data: allPayementData, refetch } = useGetAllPayementQuery();
-  const [payementInData, setPayementInData] = useState<TransactionSpecial[]>(
-    []
-  );
-  const [payementOutData, setPayementOutData] = useState<TransactionSpecial[]>(
-    []
-  );
-
   type TransactionSpecial = {
     id: number;
     type: TransactionType;
@@ -39,25 +29,29 @@ export default function ArenaAdmin() {
     };
   };
 
+  const [selectedPart, setSelectedPart] = useState<number>(1);
+  const { data: allPayementData, refetch } = useGetAllPayementQuery();
+  const [payementInData, setPayementInData] = useState<TransactionSpecial[]>(
+    []
+  );
+  const [payementOutData, setPayementOutData] = useState<TransactionSpecial[]>(
+    []
+  );
+
   useEffect(() => {
     if (allPayementData) {
       const payementIn = allPayementData.filter(
-        (payement: Transaction) => payement.type === "IN"
+        (payement: TransactionSpecial) => payement.type === "IN"
       );
       const payementOut = allPayementData.filter(
-        (payement: Transaction) => payement.type === "OUT"
+        (payement: TransactionSpecial) => payement.type === "OUT"
       );
       console.log("payementIn : ", payementIn);
-      console.log("payementOut : ", payementOut);
 
       setPayementInData(payementIn);
       setPayementOutData(payementOut);
     }
   }, [allPayementData]);
-
-  useEffect(() => {
-    refetch();
-  }, []);
 
   const handleSelect = (number: number) => {
     setSelectedPart(number);
@@ -67,6 +61,14 @@ export default function ArenaAdmin() {
     { name: "ID", uid: "id" },
     { name: "TAG", uid: "tag" },
     { name: "JETONS", uid: "token" },
+    { name: "MONTANT", uid: "euro" },
+    { name: "Utilisateur", uid: "user" },
+  ];
+
+  const columnsIN = [
+    { name: "ID", uid: "id" },
+    { name: "TAG", uid: "tag" },
+    { name: "MONTANT", uid: "amount" },
     { name: "MONTANT", uid: "euro" },
     { name: "Utilisateur", uid: "user" },
   ];
@@ -103,31 +105,40 @@ export default function ArenaAdmin() {
             {transaction?.Wallet?.User?.lastname}
           </Text>
         );
+    }
+  };
 
+  const renderCellIN = (
+    transaction: TransactionSpecial,
+    columnKey: React.Key
+  ) => {
+    switch (columnKey) {
+      case "id":
+        return <Text>{transaction?.id}</Text>;
+      case "tag":
         return (
-          <Row justify="center" align="center">
-            {/* <Col css={{ d: "flex" }}>
-              <Tooltip content="Voir les monstres">
-                <IconButton onClick={() => console.log("hello")}>
-                  <PawPrint size={20} color="#889096" weight="fill" />
-                </IconButton>
-              </Tooltip>
-            </Col> */}
-            {/* <Col css={{ d: "flex" }}>
-              <Tooltip content="Editer">
-                <IconButton onClick={() => console.log("hello")}>
-                  <Pencil size={20} color="#889096" weight="fill" />
-                </IconButton>
-              </Tooltip>
-            </Col> */}
-            <Col css={{ d: "flex" }}>
-              <Tooltip content="Supprimer">
-                <IconButton onClick={() => onDelete(arena.id)}>
-                  <Trash size={20} color="#889096" weight="fill" />
-                </IconButton>
-              </Tooltip>
-            </Col>
-          </Row>
+          <Text b size={13} css={{ tt: "capitalize", color: "$accents7" }}>
+            {transaction?.tag}
+          </Text>
+        );
+      case "token":
+        return (
+          <Text b size={13} css={{ tt: "capitalize", color: "$accents7" }}>
+            {transaction?.amount.toFixed(0)}
+          </Text>
+        );
+      case "euro":
+        return (
+          <Text b size={13} css={{ tt: "capitalize", color: "$accents7" }}>
+            {convertTokenToAmount(transaction?.amount)} €
+          </Text>
+        );
+      case "user":
+        return (
+          <Text b size={13} css={{ tt: "capitalize", color: "$accents7" }}>
+            {transaction?.Wallet?.User?.firstname}{" "}
+            {transaction?.Wallet?.User?.lastname}
+          </Text>
         );
     }
   };
@@ -138,7 +149,15 @@ export default function ArenaAdmin() {
 
       <Spacer y={1.5} />
 
-      <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }}>
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1, rotate: 360 }}
+        transition={{
+          ease: "linear",
+          duration: 1,
+          x: { duration: 0.4 },
+        }}
+      >
         <Row>
           <Button onPress={() => handleSelect(1)}>Payement entrant</Button>
           <Spacer x={1} />
@@ -149,9 +168,49 @@ export default function ArenaAdmin() {
       <Spacer y={1.5} />
 
       {selectedPart === 1 && (
-        <div>
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1, x: [null, 100, 0] }}
+          transition={{
+            ease: "linear",
+            duration: 1,
+            x: { duration: 0.4 },
+          }}
+        >
           <h3>Tableau payement entrant</h3>
-        </div>
+
+          <Table
+            aria-label="Users table"
+            css={{
+              height: "auto",
+              minWidth: "100%",
+            }}
+            selectionMode="none"
+          >
+            <Table.Header columns={columnsOUT}>
+              {(column) => (
+                <Table.Column
+                  key={column.uid}
+                  hideHeader={column.uid === "actions"}
+                  align={column.uid === "actions" ? "center" : "start"}
+                >
+                  {column.name}
+                </Table.Column>
+              )}
+            </Table.Header>
+
+            <Table.Body items={payementInData}>
+              {(item: any) => (
+                <Table.Row>
+                  {(columnKey) => (
+                    <Table.Cell>{renderCellIN(item, columnKey)}</Table.Cell>
+                  )}
+                </Table.Row>
+              )}
+            </Table.Body>
+            <Table.Pagination shadow noMargin align="center" rowsPerPage={10} />
+          </Table>
+        </motion.div>
       )}
 
       {selectedPart === 2 && (
