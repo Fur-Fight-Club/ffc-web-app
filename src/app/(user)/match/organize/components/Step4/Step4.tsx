9c61c3f450263f3dd4b4f44b1e6782dfd2e90c1a
Romain Pierucci <pierucci.romain@gmail.com>
2023-07-03 17:37:00 +0200

    feat(match): add recap step

--- src/app/(user)/match/organize/components/Step4/Step4.tsx
+++ src/app/(user)/match/organize/components/Step4/Step4.tsx
@@ -1,6 +1,7 @@
 "use client";
 
-import { Button, Row, Spacer, Text } from "@nextui-org/react";
+import { Button, Card, Col, Row, Spacer, Text } from "@nextui-org/react";
+import { convertApiTypeToType } from "@utils/utils";
 import { useRouter } from "next/navigation";
 import { useDispatch, useSelector } from "react-redux";
 import { createMatchFormState } from "src/store/matches/selector";
@@ -11,7 +12,7 @@ type Step4Props = {};
 const Step4 = (props: Step4Props) => {
   const dispatch = useDispatch();
   const router = useRouter();
-  const { monster, step, arena, bet } = useSelector(createMatchFormState);
+  const { monster, step, arena, bet, date } = useSelector(createMatchFormState);
 
   const handleNextStep = () => {
     // TODO : Go to payment page
@@ -22,12 +23,54 @@ const Step4 = (props: Step4Props) => {
   };
 
   return (
-    <div style={{ height: "86%" }}>
-      <Row>
-        <Text b size={"$3xl"}>
-          {"Récapitulatif"}
+    <div style={{ height: "80vh" }}>
+      <Spacer y={1} />
+      <Text b size={"$3xl"}>
+        {"Récapitulatif"}
+      </Text>
+      <Col>
+        <Spacer y={1} />
+        <Text b size={"$1xl"}>
+          1 - Monstre
         </Text>
-      </Row>
+        <Spacer y={0.5} />
+        <Card css={{ padding: "1rem" }}>
+          <Col>
+            <Text>Nom : {monster?.name}</Text>
+            <Text>MMR : {monster?.mmr}</Text>
+            <Text>Poids : {monster?.weight}</Text>
+            <Text>Category : {monster?.weight_category}</Text>
+            {/* @ts-ignore */}
+            <Text>Type : {convertApiTypeToType(monster?.monster_type)}</Text>
+          </Col>
+        </Card>
+        <Spacer y={1} />
+        <Text b size={"$1xl"}>
+          2 - Arène
+        </Text>
+        <Spacer y={0.5} />
+        <Card css={{ padding: "1rem" }}>
+          <Col>
+            <Text>Nom : {arena?.name}</Text>
+            <Text>
+              Adresse : {arena?.address}, {arena?.zipcode} {arena?.city},{" "}
+              {arena?.country}
+            </Text>
+            {/* @ts-ignore */}
+            <Text>Date : {date}</Text>
+          </Col>
+        </Card>
+        <Spacer y={1} />
+        <Text b size={"$1xl"}>
+          3 - Mise
+        </Text>
+        <Spacer y={0.5} />
+        <Card css={{ padding: "1rem" }}>
+          <Col>
+            <Text>Mise : {bet}</Text>
+          </Col>
+        </Card>
+      </Col>
       <Row justify="flex-end">
         <Button bordered onClick={handleStepBack}>
           Retour
