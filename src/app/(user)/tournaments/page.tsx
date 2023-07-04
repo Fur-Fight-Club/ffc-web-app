108c8679ee9f58ed57ad888e5ade57fe2f6b9682
Milan <pro@turtletv.fr>
2023-07-04 19:44:28 +0200

    💫 💄 Add animation & change container size

--- src/app/(user)/tournaments/page.tsx
+++ src/app/(user)/tournaments/page.tsx
@@ -3,6 +3,8 @@ import { Card, Grid, Text } from "@nextui-org/react";
 import { useEffect, useState } from "react";
 import { useGetAllTournamentsQuery } from "src/store/tournament/slice";
 import { TournamentItem } from "./components/TournamentItem.component";
+import { TournamentCard } from "./components/TournamentCard.component";
+import { motion } from "framer-motion";
 
 export default function Tournaments() {
   const { data: tournaments, refetch: refetchTournaments } =
@@ -25,34 +27,88 @@ export default function Tournaments() {
       css={{
         height: "100%",
       }}
+      gap={4}
     >
       {/**
        * Affichage de tous les tournois
        */}
       <Grid
         xs={12}
-        md={6}
+        md={4}
         css={{
           height: "100%",
         }}
       >
-        <Card>
-          <Card.Header>
-            <Text h3>Tous les tournois</Text>
-          </Card.Header>
-          <Card.Body>
-            {tournaments?.map((t) => (
-              <TournamentItem
-                key={t.id}
-                tournament={t}
-                selected={t.id === selectedTournament}
-                onPress={() => setSelectedTournament(t.id)}
-              />
-            ))}
-          </Card.Body>
-        </Card>
+        <motion.div
+          initial={{
+            x: "-100%",
+          }}
+          animate={{
+            x: 0,
+          }}
+          style={{
+            width: "100%",
+            height: "100%",
+          }}
+        >
+          <Card
+            css={{
+              height: "100%",
+            }}
+          >
+            <Card.Header>
+              <Text h3>Tous les tournois</Text>
+            </Card.Header>
+            <Card.Body>
+              {tournaments?.map((t) => (
+                <TournamentItem
+                  key={t.id}
+                  tournament={t}
+                  selected={t.id === selectedTournament}
+                  onPress={() => setSelectedTournament(t.id)}
+                />
+              ))}
+            </Card.Body>
+          </Card>
+        </motion.div>
       </Grid>
-      <Grid xs={12} md={6}></Grid>
+      <Grid xs={12} md={8}>
+        <motion.div
+          initial={{
+            x: "100%",
+          }}
+          animate={{
+            x: 0,
+          }}
+          style={{
+            width: "100%",
+            height: "100%",
+          }}
+        >
+          <Card
+            css={{
+              height: "100%",
+            }}
+          >
+            <Card.Header>
+              <Text h3>Informations du tournoi</Text>
+            </Card.Header>
+            <Card.Body>
+              {selectedTournament ? (
+                <TournamentCard
+                  tournament={
+                    tournaments?.find((t) => t.id === selectedTournament)!
+                  }
+                />
+              ) : (
+                <Text i>
+                  Selectionnez un tournoi pour en consulter les informations...
+                </Text>
+              )}
+            </Card.Body>
+          </Card>
+        </motion.div>
+      </Grid>
     </Grid.Container>
   );
 }
