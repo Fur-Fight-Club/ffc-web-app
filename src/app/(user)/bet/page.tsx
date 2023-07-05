3af9f7fb8c37761f084a823dad2ff50cc70a0786
Romain Pierucci <pierucci.romain@gmail.com>
2023-07-06 00:18:52 +0200

    feat(bet): add dedicated bet page

---
+++ src/app/(user)/bet/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import styles from "./page.module.scss";
+
+type BetPageProps = {};
+
+const BetPage = (props: BetPageProps) => {
+  return <h1 className={styles.betPage}>Bet Page</h1>;
+};
+
+export default BetPage;
