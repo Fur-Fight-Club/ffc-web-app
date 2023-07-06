5790573b1a60e69854a7461937426a7b3f26d24e
Romain Pierucci <pierucci.romain@gmail.com>
2023-07-06 20:05:42 +0200

    feat(join-match): add list of match in step 1

--- src/app/(user)/match/participate/components/Step1/Step1.tsx
+++ src/app/(user)/match/participate/components/Step1/Step1.tsx
@@ -1,11 +1,52 @@
 "use client";
 
+import CardList from "@components/CardList/components/CardList";
+import { Button, Row, Spacer } from "@nextui-org/react";
+import { useEffect, useState } from "react";
+import { Match } from "src/store/matches/matches.model";
+import { useGetMatchesQuery } from "src/store/matches/slice";
 import styles from "./Step1.module.scss";
 
 type Step1PropsType = {};
 
 const Step1 = (props: Step1PropsType) => {
-  return <h1 className={styles.step1}>Step 1</h1>;
+  const { data: matches, refetch } = useGetMatchesQuery();
+
+  const selectableMatches =
+    matches
+      ?.filter((match) => !match.fk_winner && !match.fk_monster_2)
+      .sort((a, b) => {
+        const dateA: any = new Date(a?.matchStartDate ?? new Date());
+
+        const dateB: any = new Date(b?.matchStartDate ?? new Date());
+        return dateB - dateA;
+      }) ?? [];
+
+  const [matchesState, setMatchesState] = useState<Match[]>(selectableMatches);
+
+  useEffect(() => {
+    refetch();
+    setMatchesState(selectableMatches);
+  }, []);
+
+  return (
+    <div className={styles.step1}>
+      <Spacer y={1} />
+      <div className={styles.cardList}>
+        <CardList>
+          {matchesState?.map((match) => (
+            <CardList.MatchItem key={match.id} match={match} />
+          ))}
+        </CardList>
+      </div>
+      <Spacer y={1} />
+      <Row justify="flex-end">
+        <Button bordered>Retour</Button>
+        <Spacer x={0.5} />
+        <Button>Suivant</Button>
+      </Row>
+    </div>
+  );
 };
 
 export default Step1;
--- src/components/CardList/components/MatchItem/MatchItem.tsx
+++ src/components/CardList/components/MatchItem/MatchItem.tsx
@@ -1,9 +1,11 @@
 "use client";
 
 import { CardListContext } from "@components/CardList/cardlist-context";
-import { Text } from "@nextui-org/react";
-import colors from "@styles/_colors.module.scss";
+import { Avatar, Badge, Grid, Text } from "@nextui-org/react";
 import { mergeClassNames } from "@utils/main";
+import { textColor, weightCategoryColors } from "@utils/utils";
+import { format } from "date-fns";
+import fr from "date-fns/locale/fr";
 import { motion } from "framer-motion";
 import { useContext } from "react";
 import { Match } from "src/store/matches/matches.model";
@@ -28,21 +30,105 @@ const MatchItem = ({ match, onClick, isSelected = false }: MatchItemProps) => {
   return (
     <motion.div
       className={mergeClassNames([
-        styles.MatchItem,
+        styles.arenaItem,
         isMatchSelected && styles.arenaItemActive,
       ])}
       whileHover={{ scale: 1.05 }}
       whileTap={{ scale: 0.5 }}
       onClick={handleOnClick}
     >
-      <div className={styles.infoContainer}>
-        <Text b color={isMatchSelected ? colors.white : colors.black}>
-          pouet
-        </Text>
-        <Text color={isMatchSelected ? colors.white : colors.black}>
-          monster {match.Monster1.name}
-        </Text>
+      <div className={styles.date}>
+        {"Le "}
+        {format(
+          // @ts-ignore
+          new Date(match?.matchStartDate ?? new Date()),
+          "dd/MM/yyyy à HH:mm",
+          {
+            locale: fr,
+          }
+        )}
       </div>
+      <Grid xs={12} className={styles.matchItem}>
+        <Grid.Container gap={2}>
+          <Grid xs={6} justify="center" alignItems="center" direction="column">
+            <Avatar src={match.Monster1?.picture} css={{ size: "$20" }} />
+            <Text h4>{match.Monster1?.name}</Text>
+            <div>
+              <Badge
+                css={{
+                  backgroundColor: weightCategoryColors(
+                    match.Monster1?.weight_category
+                  ),
+                  color: textColor(
+                    weightCategoryColors(match.Monster1?.weight_category)
+                  ),
+                }}
+              >
+                {match.Monster1?.weight}kg
+              </Badge>
+              <Badge>
+                {match.Monster1?.mmr}
+                {match.Monster1?.mmr ? " MMR" : "—"}
+              </Badge>
+            </div>
+          </Grid>
+          <Grid
+            xs={6}
+            justify="center"
+            alignItems="center"
+            direction="column"
+            css={{ cursor: "pointer" }}
+          >
+            <Avatar
+              src={match.Monster2?.picture}
+              css={{
+                size: "$20",
+                cursor: match.Monster2 === undefined ? "pointer" : "default",
+              }}
+              text="+"
+              onClick={match.Monster2 === undefined ? () => {} : undefined}
+            />
+            <Text h4>{match.Monster2?.name}</Text>
+            <div>
+              <Badge
+                css={{
+                  backgroundColor: weightCategoryColors(
+                    match.Monster2?.weight_category
+                  ),
+                  color: textColor(
+                    weightCategoryColors(match.Monster2?.weight_category)
+                  ),
+                }}
+              >
+                {match.Monster2?.weight}
+                {match.Monster2?.weight ? "kg" : "—"}
+              </Badge>
+              <Badge>
+                {match.Monster2?.mmr}
+                {match.Monster2?.mmr ? " MMR" : "—"}
+              </Badge>
+            </div>
+          </Grid>
+          {match.fk_winner && (
+            <Grid
+              xs={12}
+              justify="center"
+              alignItems="center"
+              direction="column"
+            >
+              <Text h4 color="green">
+                Match terminé !
+              </Text>
+              <Text h5>
+                {match.fk_winner === match.Monster1?.id
+                  ? match.Monster1?.name
+                  : match.Monster2?.name}{" "}
+                a gagné !
+              </Text>
+            </Grid>
+          )}
+        </Grid.Container>
+      </Grid>
     </motion.div>
   );
 };
