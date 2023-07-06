1d4c9c14e1236e25b6397ce6104f42591588c954
Romain Pierucci <pierucci.romain@gmail.com>
2023-07-06 15:37:49 +0200

    feat(join-match): add stepper

--- src/app/(user)/match/participate/page.tsx
+++ src/app/(user)/match/participate/page.tsx
@@ -1,7 +1,53 @@
-type ParticipateMatchPageProps = {};
+"use client";
 
-const ParticipateMatchPage = (props: ParticipateMatchPageProps) => {
-  return <h1>Participate Match Page</h1>;
+import { Steps } from "antd";
+import { useDispatch, useSelector } from "react-redux";
+import { joinMatchFormState } from "src/store/matches/selector";
+import { resetJoinForm } from "src/store/matches/slice";
+
+type OrganizeMatchPageProps = {};
+
+const OrganizeMatchPage = (props: OrganizeMatchPageProps) => {
+  const { step } = useSelector(joinMatchFormState);
+  const dispatch = useDispatch();
+
+  const renderStepperItem = (step: number) => (
+    <Steps
+      size="small"
+      current={step}
+      items={[
+        {
+          title: "Match",
+        },
+        {
+          title: "Monstre",
+        },
+        {
+          title: "Récapitulatif",
+        },
+        {
+          title: "Terminé",
+        },
+      ]}
+    />
+  );
+
+  const renderStepPage = (step: number) => {
+    switch (step) {
+      case 0:
+        return <div>step 1 JOIN</div>;
+      default:
+        dispatch(resetJoinForm());
+        return <div>default steps forms</div>;
+    }
+  };
+
+  return (
+    <div>
+      {renderStepperItem(step)}
+      {renderStepPage(step)}
+    </div>
+  );
 };
 
-export default ParticipateMatchPage;
+export default OrganizeMatchPage;
