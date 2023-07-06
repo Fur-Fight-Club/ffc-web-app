4a8cb845341b524c52aa6ed6a9ed459cc5ba6f7d
Romain Pierucci <pierucci.romain@gmail.com>
2023-07-06 20:41:57 +0200

    feat(join-match): match preview in step 1

--- src/app/(user)/match/participate/components/Step1/Step1.tsx
+++ src/app/(user)/match/participate/components/Step1/Step1.tsx
@@ -5,6 +5,8 @@ import { Button, Row, Spacer } from "@nextui-org/react";
 import { useEffect, useState } from "react";
 import { Match } from "src/store/matches/matches.model";
 import { useGetMatchesQuery } from "src/store/matches/slice";
+import { Monster } from "src/store/monsters/monsters.model";
+import MonsterCardPreview from "../../../organize/components/MonsterCardPreview/MonsterCardPreview";
 import styles from "./Step1.module.scss";
 
 type Step1PropsType = {};
@@ -23,6 +25,16 @@ const Step1 = (props: Step1PropsType) => {
       }) ?? [];
 
   const [matchesState, setMatchesState] = useState<Match[]>(selectableMatches);
+  const [monsterOpponent, setMonsterOpponent] = useState<Monster | null>(null);
+
+  const handleMatchClick = (monster: Monster) => {
+    if (monsterOpponent?.id !== monster.id) {
+      setMonsterOpponent(monster);
+      return;
+    }
+
+    setMonsterOpponent(null);
+  };
 
   useEffect(() => {
     refetch();
@@ -32,12 +44,29 @@ const Step1 = (props: Step1PropsType) => {
   return (
     <div className={styles.step1}>
       <Spacer y={1} />
-      <div className={styles.cardList}>
-        <CardList>
-          {matchesState?.map((match) => (
-            <CardList.MatchItem key={match.id} match={match} />
-          ))}
-        </CardList>
+      <div className={styles.content}>
+        <div className={styles.cardList}>
+          <CardList>
+            {matchesState?.map((match) => (
+              <CardList.MatchItem
+                key={match.id}
+                match={match}
+                // @ts-ignore
+                onClick={() => handleMatchClick(match.Monster1)}
+              />
+            ))}
+          </CardList>
+        </div>
+        <Spacer x={1} />
+        <div className={styles.previews}>
+          {/* @ts-ignore */}
+          <MonsterCardPreview
+            monster={monsterOpponent}
+            labelPreview="Adversaire"
+          />
+          <Spacer y={1} />
+          <MonsterCardPreview labelPreview="Vous" />
+        </div>
       </div>
       <Spacer y={1} />
       <Row justify="flex-end">
